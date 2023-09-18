5bee165c88239f26a4dc3d40ba33f5b420f9cd5c
Bernardo Marçal <bernardo.amaral.2001@gmail.com>
2023-09-18 14:52:40 +0100

    Dump inicial

---
+++ sndfile-example-src/build/CMakeFiles/wav_cp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/mnt/c/Users/benny/Desktop/ECT/4º Ano/IC/final_project/sndfile-example-bin/wav_cp"
+  "/mnt/c/Users/benny/Desktop/ECT/4º Ano/IC/final_project/sndfile-example-bin/wav_cp.pdb"
+  "CMakeFiles/wav_cp.dir/wav_cp.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wav_cp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
